94084c349d5983761eba125a04a21994024465ca
guoarthur <arthur.guo@uwaterloo.ca>
2017-03-04 17:32:40 -0500

    Add frontend and hook up to backend

---
+++ frontend/src/views/charts/DoughnutExample.vue
@@ -0,0 +1,13 @@
+<script>
+import { Doughnut, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+
+export default Doughnut.extend({
+  mixins: [reactiveProp],
+  props: ['options'],
+  mounted () {
+    this.renderChart(this.chartData, {responsive: true, maintainAspectRatio: false})
+  }
+})
+</script>
